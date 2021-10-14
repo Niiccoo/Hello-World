35eb27b2afed6921fb94aa5e035e1cf41351dc1d
Niiccoo <37897752+Niiccoo@users.noreply.github.com>
2021-10-14 15:48:17 +0200

    Update show.py

--- show.py
+++ show.py
@@ -1,1 +1,17 @@
 print("Hello Python World")
+
+import kivy
+kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello kivy world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
